2ae34d1a0ef4e3004f4208379ff21d181b37f3cf
jonaprojects <merkava234@gmail.com>
2024-10-08 20:40:23 +0300

    updating the next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export const nextConfig = {
+  output: "export",
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
-
-export default nextConfig;
